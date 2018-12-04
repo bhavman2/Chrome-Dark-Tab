d1adff41b39eead8cfcb225dab2750894f2a6243
Bhavik Raval <bhavman2@gmail.com>
2018-12-04 12:41:09 -0800

    Version 1.2

--- background.js
+++ background.js
@@ -1,14 +1,1 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.storage.sync.get('isExtensionOn', function (data) {
-    if (data.isExtensionOn === true) {
-      chrome.storage.sync.set({ isExtensionOn: false });
-      chrome.browserAction.setIcon({ path: 'light-off-16.png' });
-      // chrome.browserAction.setBadgeText({ text: '' });
-    } else {
-      chrome.storage.sync.set({ isExtensionOn: true });
-      chrome.browserAction.setIcon({ path: 'light-on-16.png' });
-      // chrome.browserAction.setBadgeBackgroundColor({ color: 'black' });
-      // chrome.browserAction.setBadgeText({ text: 'ON' });
-    }
-  });
-});
+chrome.browserAction.onClicked.addListener(function (tab) { chrome.storage.sync.get('isExtensionOn', function (data) { if (data.isExtensionOn === true) { chrome.storage.sync.set({ isExtensionOn: false }); chrome.browserAction.setIcon({ path: 'light-off-16.png' }); } else { chrome.storage.sync.set({ isExtensionOn: true }); chrome.browserAction.setIcon({ path: 'light-on-16.png' }); } }); });
